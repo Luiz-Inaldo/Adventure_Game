5792cc0bd4434cc7e16b48a62c85fcde644aa85b
luiz inaldo <luiz.inaldo970@gmail.com>
2024-02-24 21:06:59 -0300

    feat: implementando um pouco mais da lógica da venda do market

--- src/scripts/market.js
+++ src/scripts/market.js
@@ -11,6 +11,9 @@ const showcase = document.getElementById('items-showcase');
 const itemJsonListEndpoint = "/src/json/items.json";
 let itemList;
 
+// variáveis de audio
+const purchaseSound = new Audio('/src/assets/audio/purchase.wav');
+
 /*Recebendo informações atualizadas do localStorage*/
 const infoPlayer = JSON.parse(localStorage.getItem('jogador'))
 
@@ -39,41 +42,74 @@ async function displayBuyableItems(endpoint) {
         .then(items => {
             itemList = items;
             const {allItems} = itemList;
+            const armaduras = allItems.filter(element => {return element.tipo === "proteção"})
+            const armas = allItems.filter(element => {return element.tipo === "ataque"})
+            const suporte = allItems.filter(element => {return element.tipo === "suporte"})
+
             refreshMarket()
+
             showcase.innerHTML = `
                 <h2 id="showcase-title">Comprar</h2>
             `
-            
-            for (const item of allItems) {
-                
-                if (item.disponivel && item.tipo !== "suporte") {
-                    showcase.innerHTML += `
-                    
-                        <li class="mkt-item">
-                            <img src="${item.icon}" alt="armor-icon" class="mkt-item-icon">
-                            <span class="mkt-item-description">${item.nome}</span>
-                            <span class="mkt-item-attribute">+ ${item.atributo} ${item.tipo}</span>
-                            <span class="mkt-item-value">${item.valor}G</span>
-                            <button class="buy-item rpgui-button">comprar</button>
-                        </li>
-                    
-                    `
-                } else if (item.disponivel && item.tipo === "suporte") {
 
+            for (const armadura of armaduras) {
+                
+                if (armadura.disponivel) {
+                    
                     showcase.innerHTML += `
                     
                         <li class="mkt-item">
-                            <img src="${item.icon}" alt="armor-icon" class="mkt-item-potion-icon">
-                            <span class="mkt-item-description">${item.nome}</span>
-                            <span class="mkt-item-attribute">${item.descricao}</span>
-                            <span class="mkt-item-value">${item.valor}G</span>
-                            <button class="buy-item rpgui-button">comprar</button>
+                            <img src="${armadura.icon}" alt="armor-icon" class="mkt-item-icon">
+                            <span class="mkt-item-description">${armadura.nome}</span>
+                            <span class="mkt-item-attribute">+ ${armadura.atributo} ${armadura.tipo}</span>
+                            <span class="mkt-item-value">${armadura.valor}G</span>
+                            <button id="${armadura.id}" class="buy-item rpgui-button">comprar</button>
                         </li>
+                 
+                    `
+
+                }
+
+            }
+
+            for (const arma of armas) {
+                
+                if (arma.disponivel) {
                     
+                    showcase.innerHTML += `
+                    
+                        <li class="mkt-item">
+                            <img src="${arma.icon}" alt="armor-icon" class="mkt-item-icon">
+                            <span class="mkt-item-description">${arma.nome}</span>
+                            <span class="mkt-item-attribute">+ ${arma.atributo} ${arma.tipo}</span>
+                            <span class="mkt-item-value">${arma.valor}G</span>
+                            <button id="${arma.id}" class="buy-item rpgui-button">comprar</button>
+                        </li>
+                 
+                    `
+
+                }
+
+            }
+
+            for (const supplie of suporte) {
+                
+                if (supplie.disponivel) {
+                    
+                    showcase.innerHTML += `
+                    
+                        <li class="mkt-item">
+                            <img src="${supplie.icon}" alt="armor-icon" class="mkt-item-potion-icon">
+                            <span class="mkt-item-description">${supplie.nome}</span>
+                            <span class="mkt-item-attribute">${supplie.descricao}</span>
+                            <span class="mkt-item-value">${supplie.valor}G</span>
+                            <button id="${supplie.id}" class="buy-item rpgui-button">comprar</button>
+                        </li>
+                      
                     `
                     
                 }
-                
+
             }
 
         })
@@ -129,16 +165,31 @@ function refreshMarket() {
 }
 
 // funções de compra e venda
-showcase.addEventListener('click', async (click) => {
-    console.log(itemList);
-    const button = click.target;
+showcase.addEventListener('click', async (button) => {
 
-    if (button.classList.contains('buy-item')) {
-        const elementName = button.parentElement
-        .querySelector('.mkt-item-description').textContent;
+    if (button.target.classList.contains('buy-item')) {
         
-        
+        const buttonID = parseInt(button.target.id);
+        const itemToBuy = itemList.allItems.filter(element => {return element.id === buttonID});
+        const itemValue = itemToBuy[0].valor;
+
+        if (infoPlayer[0].gold >= itemValue) {
+
+            purchaseSound.play();
+            infoPlayer[2].push(itemToBuy);
+            
+        } else {
+
+            console.log("não tem dinheiro");
+
+        }
         
     }
 
 })
+
+async function updatePlayerGold() {
+
+
+
+}
